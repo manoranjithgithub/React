5fe0ee0036f60bc35475da5b6830fbaa18b94152
manoranjith <manoranjith@dilligentech.com>
2025-12-26 14:58:47 +0530

    index.js page changes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import "../assets/css/styles.css";
-
-const root = createRoot(document.getElementById("root"));
-root.render(<App />);
+ 
+ReactDOM.render(<App />, document.getElementById("root"));
